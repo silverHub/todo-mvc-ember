2e5eb827802f36bffb37f61dccc7417075df8bc1
Tamas Hoffman <tms.hoffman@gmail.com>
2016-06-27 09:13:52 +0200

    some gotcha

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -2,15 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName:'li',
-  classNameBindings: ['completed'],
+  classNameBindings: ['completed','editing'],
   completed: false,
+  editing: false,
   actions:{
-    complete() {
-      console.log(this);
+    completeTodo(item) {
+      console.log(this, arguments);
       this.toggleProperty('completed');
+      this.sendAction('complete', item);
     },
-    delete() {
-      console.log('deleted');
+    changeValue(){
+      if (event.keyCode === 13) {
+          this.toggleProperty('editing');
+      }
+    },
+    deleteTodo(item) {
+      console.log('delete action in component');
+      this.sendAction('delete', item);
+    },
+    edit(){
+      console.log('edit action in component');
+      this.toggleProperty('editing');
     }
+
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,26 @@
 import Ember from 'ember';
 
 let todos = [
-  { title: 'Todo1'},
-  { title: 'Todo2'},
-  { title: 'Todo3'},
+  {id: 1, title: 'Todo1'},
+  {id: 2, title: 'Todo2'},
+  {id: 3, title: 'Todo3'},
 ];
 
 export default Ember.Route.extend({
   model(){
     return todos;
+  },
+  actions:{
+    deleteItem(todo){
+      let index = todos.indexOf(todo);
+      console.log('delete action in router', todo, index);
+      todos.splice(index,1);
+    },
+    completeItem(todo){
+      let completedTodo = todos.find(x => x.id === todo.id);
+      completedTodo.completed = true;
+      console.log('complete action in router', todo, todos);
+    }
   }
 
 });
